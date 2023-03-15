1c211aa640f6cd6d43b59cce729a82d39a1890d3
LyuboDikov <lyubo.dikov@icloud.com>
2023-03-15 14:38:38 +0200

    Add Exercise Solution
    Added first task of Exercise

---
+++ ReflectionAndAnnotation/Exercise/HarvestingFields/Main.java
@@ -0,0 +1,46 @@
+package ReflectionAndAnnotation.Exercise.HarvestingFields;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Class<RichSoilLand> clazz = RichSoilLand.class;
+
+        Field[] fields = clazz.getDeclaredFields();
+
+        Consumer<Field> printer = field -> System.out.printf("%s %s %s%n",
+                Modifier.toString(field.getModifiers()), field.getType().getSimpleName(), field.getName());
+
+        String inputCommand = scanner.nextLine();
+
+        while (!"HARVEST".equals(inputCommand)) {
+
+            switch (inputCommand) {
+                case "private":
+                    Arrays.stream(fields).filter(field -> Modifier.isPrivate(field.getModifiers()))
+                            .forEach(printer);
+                    break;
+                case "protected":
+                    Arrays.stream(fields).filter(field -> Modifier.isProtected(field.getModifiers()))
+                            .forEach(printer);
+                    break;
+                case "public":
+                    Arrays.stream(fields).filter(field -> Modifier.isPublic(field.getModifiers()))
+                            .forEach(printer);
+                    break;
+                case "all":
+                    Arrays.stream(fields).forEach(printer);
+                    break;
+            }
+            
+            inputCommand = scanner.nextLine();
+        }
+    }
+}
